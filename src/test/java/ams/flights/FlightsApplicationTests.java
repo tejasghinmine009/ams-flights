f7da4fac5c153b924d01d59eec39d64e56103ef0
TEJAS GHINMINE <tejasghinmine009@gmail.com>
2023-09-17 18:20:27 +0530

    first commit

---
+++ src/test/java/ams/flights/FlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package ams.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
